f320a88c33aa4940724ebbdeec6ee482e75fe4f1
kodyclemens <kody.clemens@gmail.com>
2018-11-12 19:31:25 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(possible_anagrams)
+    possible_anagrams.map do |anagram|
+      if word.split("").sort == anagram.split("").sort
+        anagram
+      end
+    end.compact
+  end
+end
