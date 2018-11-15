c000778fb415787785dfaf86ac2d811ecc4a0d84
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-11-15 19:40:13 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word     
+  end 
+  
+  def match(possibleAnagrams)
+    anagrams = []
+    possibleAnagrams.each do |possibleAnagram|
+      if @word.split("").sort == possibleAnagram.split("").sort
+        anagram << possibleAnagram
+      end 
+    end 
+    anagrams
+  end 
+end 
