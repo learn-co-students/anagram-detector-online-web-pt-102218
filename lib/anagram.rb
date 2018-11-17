f1b17a234dcf89b01e7bb220ebff70f8ff6ce785
Corina Talavera <corina.talavera@gmail.com>
2018-11-17 14:38:12 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :anagram
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(word)
+    words.select do |word|
+      possible_anagram(word)
+    end
+  end
+
+  def possible_anagram(word)
+    word.chars.sort == @anagram.chars.sort
+  end
+end
+allergy = Anagram.new("allergy")
+allergy.match(%w(gallery ballerina regally clergy largely leading))
