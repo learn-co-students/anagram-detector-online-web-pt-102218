c082c2257356503201080d331bfcb07b70dc27ca
Brandon Teich <brandonteich123@gmail.com>
2018-11-19 23:33:20 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
+   def match(newarray)
+    newarray.find_all do |x| 
+      x.split("").sort == @name.split("").sort
+    end
+  end
+end
